bug fix in descriptor coords homography calculation

utils.py
@@ -667,18 +667,17 @@ def descriptor_loss_2(descriptor: torch.Tensor, descriptor_warped: torch.Tensor,
         valid_mask = valid_mask.unsqueeze(1)
     coords = torch.stack(torch.meshgrid(torch.linspace(0, Wc, Wc), torch.linspace(0, Hc, Hc)), dim=2)  # coordinates of Hc, Wc  grid
     coords = coords * 8 + 8 // 2  # to get the center coordinates of respective expanded image with (H,W)
-    coords = coords.transpose(1, 0)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    warped_coord = warp_points(coords.reshape([-1, 2]), homography).reshape([batch_size, Hc, Wc, 1, 1, 2])
-    coords = torch.cat([coords.unsqueeze(0)]*batch_size, dim=0).reshape([batch_size, 1, 1, Hc, Wc, 2])
+    warped_coord = warp_points(coords.reshape([-1, 2]), homography).reshape([batch_size, Hc*Wc, 1,  2])
+    coords = torch.cat([coords.unsqueeze(0)]*batch_size, dim=0).reshape([batch_size, 1, Hc*Wc, 2])
     mask3D = labels2Dto3D(valid_mask, cell_size=8, add_dustbin=False, device=torch.device(device)).float()
     mask3D = torch.prod(mask3D, dim=1).to(device)
     cell_dist = np.linalg.norm(coords - warped_coord, axis=-1)
     mask = cell_dist <= threshold
-    mask = torch.from_numpy(mask.astype(np.float32)).reshape([batch_size, Hc * Wc, Hc * Wc]).to(device)
+    mask = torch.from_numpy(mask.astype(np.float32)).to(device)
     new_desc = descriptor.reshape((batch_size, -1, size))
-    new_warp_descriptor = descriptor_warped.reshape((batch_size, -1, size))
-    desc_product = torch.matmul(new_desc, new_warp_descriptor.transpose(2, 1))
+    new_warp_descriptor = descriptor_warped.reshape((batch_size, size, -1))
+    desc_product = torch.matmul(new_desc, new_warp_descriptor)
     positive_corr = torch.max(margin_pos * torch.ones_like(desc_product) - desc_product,
                               torch.zeros_like(desc_product))
     negative_corr = torch.max(desc_product - margin_neg * torch.ones_like(desc_product),